77c178e743e4458aa9888e0ba343f203e557a57a
Xuzihan233 <zxu909@students.fairmontschools.com>
2025-02-14 20:46:20 -0800

    Update Array.java

--- Array.java
+++ Array.java
@@ -1,48 +1,94 @@
 public class Array
 {
 
-	// Array should be of format
-	// 1 2 3
-	// 4 5 6
-	// 7 8 9
-	public int[][] getRowMajorSquare(int x)
-	{
-//	
-	}
-
-	// Array should be of format
-	// 1 4 7
-	// 2 5 8
-	// 3 6 9
-	public int[][] getColumnMajorSquare(int x)
-	{
-//		
-	}
-
-	// Array should be of format
-	// 1 2 3 4
-	// 5 6 7 8
-	// 9 10 11 12
-	public int[][] getRowMajorRectangle(int row, int column)
-	{
+	public static void main(String[] args) {
+		
+		System.out.println("getRowMajorSquare");
+		int [][] arr = getRowMajorSquare(3);
+		
+		printDouble(arr);
+		
+		System.out.println("getRowMajorRectangle");
+		printDouble( getRowMajorRectangle(3,4) );
+		
+		System.out.println("getColumnMajorSquare");
+		printDouble(getColumnMajorSquare(3));
+		
+		System.out.println("getColumnMajorRectangle");
+		printDouble(getColumnMajorRectangle(3,4));
+		
 		
 	}
-
-	// Array should be of format
-	// 1 4 7 10
-	// 2 5 8 11
-	// 3 6 9 12
-	public int[][] getColumnMajorRectangle(int column, int row)
-	{
 	
+	public static int[][] getRowMajorSquare(int x){
+		
+		int [][] arr = new int [x][x]; // 3 * 3
+		
+		for (int i = 0; i < arr.length; i++){
+			for (int j = 0; j < arr[0].length; j++){
+				arr[i][j] =  j + 1 + i * 3;
+			}
+		}
+
+		return arr;
+	}
+	
+	public static int[][] getColumnMajorSquare(int x){
+		// 1 4 7
+		// 2 5 8
+		// 3 6 9
+		int [][] arr = new int [x][x];
+		
+		for (int i = 0; i < arr.length; i++){
+			for (int j = 0; j < arr[0].length; j++){
+				arr[i][j] =  i + j * 3 + 1;
+			}
+		}
+		
+		return arr;
+	}
+	
+	
+	public static int[][] getRowMajorRectangle(int row, int column){
+		// 1  2  3  4
+		// 5  6  7  8
+		// 9 10 11 12
+		// row : arr.length
+		// column: arr[0].length
+		
+		int [][] arr = new int [row][column]; // 3 * 4
+		
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[0].length; j++) {
+				arr[i][j] = i * 4 + j + 1;
+			}
+		}
+		
+		return arr;
+	}
+	
+	public static int[][] getColumnMajorRectangle(int row, int column){
+		
+		int [][] arr = new int [row][column]; // 3 * 4
+		
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[0].length; j++) {
+				arr[i][j] = j * 3 + i + 1;
+			}
+		}
+		
+		return arr;
+	}
+	
+	
+	public static void printDouble(int matrix [][]){
+		for (int [] row : matrix) {
+			for (int elem : row) {
+				System.out.print(elem+" ");
+			}
+			System.out.println();
+		}
 	}
 
-	// Please note that each digit occupies two spaces and there is a space between
-	// them.
-	// You are not required to deal with three digit numbers.
-	public void printDouble(int[][] a)
-    {
-	
 
-	}
 }
